71f2573723b92acbbd780932bdd2cf4f6bd261fc
Pedro Andrés Chaparro Quintero <pedroandreschaparro@gmail.com>
2024-03-05 17:54:08 -0500

    feat: :sparkles: Add base mobile navbar

--- src/routes/__root.tsx
+++ src/routes/__root.tsx
@@ -1,11 +1,14 @@
-import { DesktopNavbar } from '@/components/navbar/desktop/desktopNavbar';
 import { createRootRoute, Outlet } from '@tanstack/react-router';
 import { TanStackRouterDevtools } from '@tanstack/router-devtools';
+
+import { DesktopNavbar } from '@/components/navbar/desktop/desktopNavbar';
+import { MobileNavbar } from '@/components/navbar/mobile/mobileNavbar';
 
 export const Route = createRootRoute({
   component: () => (
     <>
       <DesktopNavbar />
+      <MobileNavbar />
       <Outlet />
       <footer className='container'>Shared footer</footer>
       <TanStackRouterDevtools position='bottom-right' />
