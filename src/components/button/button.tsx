8dbb7234598c9b02d6b1f0379aeeee8a285e5a0c
Pedro Andrés Chaparro Quintero <pedroandreschaparro@gmail.com>
2024-03-05 17:52:37 -0500

    feat: :lipstick: Add new variants to button component
    Add new visual variant (outline) and introduce size variants.

--- src/components/button/button.tsx
+++ src/components/button/button.tsx
@@ -6,11 +6,17 @@ export const buttonVariantClassnames = cva(Styles.button, {
   variants: {
     variant: {
       primary: Styles['button--primary'],
+      outline: Styles['button--outline'],
+    },
+    size: {
+      normal: Styles['button--normal'],
+      icon: Styles['button--icon'],
     },
   },
   // compoundVariants: [],
   defaultVariants: {
     variant: 'primary',
+    size: 'normal',
   },
 });
 
@@ -20,13 +26,14 @@ interface ButtonProps
 
 export const Button = ({
   variant,
+  size,
   className,
   children,
   ...props
 }: ButtonProps) => {
   return (
     <button
-      className={buttonVariantClassnames({ variant, className })}
+      className={buttonVariantClassnames({ variant, size, className })}
       {...props}
     >
       {children}
