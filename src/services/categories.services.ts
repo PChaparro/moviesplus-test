d8cdf0bd4626a5f79648b4ba3db859c314a6a76d
Pedro Andrés Chaparro Quintero <pedroandreschaparro@gmail.com>
2024-03-06 16:03:10 -0500

    chore: :clown_face: Add categories mocks

--- src/services/categories.services.ts
+++ src/services/categories.services.ts
@@ -1,8 +1,66 @@
-import { Category } from '@/types/definitions';
+import { Category, Movie } from '@/types/definitions';
+import { getRandomNumber } from '@/utils/utils';
 
+import adventureMovies from '@/data/categories/12.json';
+import animationMovies from '@/data/categories/16.json';
+import actionMovies from '@/data/categories/28.json';
+import comedyMovies from '@/data/categories/35.json';
+import historyMovies from '@/data/categories/36.json';
 import categoriesMockData from '@/data/categories/categories.json';
 
 export async function getCategoriesService(): Promise<Category[]> {
-  await new Promise((resolve) => setTimeout(resolve, 1000));
+  await new Promise((resolve) =>
+    setTimeout(resolve, getRandomNumber({ min: 500, max: 1000 })),
+  );
+
   return categoriesMockData;
+}
+
+type categoryMockData = {
+  id: number;
+  name: string;
+  pages: Movie[][];
+};
+
+const categoriesMoviesMockData: Record<string, categoryMockData> = {
+  '12': adventureMovies,
+  '16': animationMovies,
+  '28': actionMovies,
+  '35': comedyMovies,
+  '36': historyMovies,
+};
+
+interface CategoryMoviesResponse {
+  id: string;
+  name: string;
+  movies: Movie[];
+  next: number | null;
+}
+
+export async function getMoviesByCategoryService(
+  categoryId: string,
+  page: number = 1,
+): Promise<CategoryMoviesResponse> {
+  await new Promise((resolve) =>
+    setTimeout(resolve, getRandomNumber({ min: 500, max: 1000 })),
+  );
+
+  if (!categoriesMoviesMockData[categoryId]) {
+    throw new Error('Category not found');
+  }
+
+  const categoryData = categoriesMoviesMockData[categoryId];
+
+  const requiredPageExists = categoryData.pages.length >= page;
+  if (!requiredPageExists) throw new Error('Page not found');
+
+  const movies = categoryData.pages[page - 1];
+  const hasNextPage = categoryData.pages.length > page;
+
+  return {
+    id: categoryId,
+    name: categoryData.name,
+    movies,
+    next: hasNextPage ? page + 1 : null,
+  };
 }
--- src/types/definitions.ts
+++ src/types/definitions.ts
@@ -3,3 +3,17 @@ export type Category = {
   name: string;
   thumbnail: string;
 };
+
+export type Movie = {
+  id: number;
+  overview: string;
+  poster_path: string;
+  release_date: string;
+  title: string;
+  vote_average: number;
+  vote_count: number;
+  genres: {
+    id: number;
+    name: string;
+  }[];
+};
