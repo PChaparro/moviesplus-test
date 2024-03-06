4688fcf5b7d1dcaee6e5134b20e13c20ce3657ba
Pedro Andrés Chaparro Quintero <pedroandreschaparro@gmail.com>
2024-03-06 13:52:12 -0500

    refactor: :coffin: Remove unused classes and variables from global css file

--- src/components/home/categories-section/categoriesSection.tsx
+++ src/components/home/categories-section/categoriesSection.tsx
@@ -6,7 +6,7 @@ export const CategoriesSection = () => {
   const { categories, isLoading } = UseCategories();
 
   return (
-    <section id='categories'>
+    <section id='categories' className='mt-8'>
       <h2 className='text-4xl font-semibold text-white'>Categories</h2>
       <CategoriesGrid isLoading={isLoading} categories={categories} />
     </section>
