f9e591c123d910e0dd07859f2d77fb5ad4f9fd65
Pedro Andrés Chaparro Quintero <pedroandreschaparro@gmail.com>
2024-03-05 21:03:24 -0500

    chore: :heavy_plus_sign: Install tanstack query and tanstack query devtools

--- src/routes/__root.tsx
+++ src/routes/__root.tsx
@@ -1,17 +1,29 @@
+import { QueryClient, QueryClientProvider } from '@tanstack/react-query';
+import { ReactQueryDevtools } from '@tanstack/react-query-devtools';
 import { createRootRoute, Outlet } from '@tanstack/react-router';
 import { TanStackRouterDevtools } from '@tanstack/router-devtools';
 
 import { DesktopNavbar } from '@/components/navbar/desktop/desktopNavbar';
 import { MobileNavbar } from '@/components/navbar/mobile/mobileNavbar';
 
+const queryClient = new QueryClient({
+  defaultOptions: {
+    queries: {
+      refetchOnWindowFocus: false,
+      refetchOnReconnect: false,
+    },
+  },
+});
+
 export const Route = createRootRoute({
   component: () => (
-    <>
+    <QueryClientProvider client={queryClient}>
       <DesktopNavbar />
       <MobileNavbar />
       <Outlet />
       <footer className='container'>Shared footer</footer>
-      <TanStackRouterDevtools position='bottom-right' />
-    </>
+      <TanStackRouterDevtools position='bottom-left' />
+      <ReactQueryDevtools position='right' />
+    </QueryClientProvider>
   ),
 });
