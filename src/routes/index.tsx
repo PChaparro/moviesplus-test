6b20003169cf17b449038d7cb1f387cd5a190542
Pedro Andrés Chaparro Quintero <pedroandreschaparro@gmail.com>
2024-03-07 20:08:17 -0500

    refactor: :recycle: Create movie header component

--- src/routes/index.tsx
+++ src/routes/index.tsx
@@ -1,7 +1,9 @@
 import { createFileRoute } from '@tanstack/react-router';
+import { PlayIcon } from 'lucide-react';
 
 import { CategoriesSection } from '@/components/home/categories-section/categoriesSection';
-import { FeaturedMovieHeader } from '@/components/home/header/featuredMovieHeader';
+import { MovieHeader } from '@/components/movie/MovieHeader';
+import { Button } from '@/components/shared/button/button';
 
 export const Route = createFileRoute('/')({
   component: Home,
@@ -10,7 +12,16 @@ export const Route = createFileRoute('/')({
 function Home() {
   return (
     <main className='container'>
-      <FeaturedMovieHeader />
+      <MovieHeader
+        title='Dune: Part Two'
+        overview='Follow the mythic journey of Paul Atreides as he unites with Chani and the Fremen while on a path of revenge against the conspirators who destroyed his family. Facing a choice between the love of his life and the fate of the known universe, Paul endeavors to prevent a terrible future only he can foresee.'
+        backdrop_path='/xOMo8BRK7PfcJv9JCnx7s5hj0PX.jpg'
+      >
+        <Button>
+          <PlayIcon className='mr-2' />
+          View now
+        </Button>
+      </MovieHeader>
       <CategoriesSection />
     </main>
   );
